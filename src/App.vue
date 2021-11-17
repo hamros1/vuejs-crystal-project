7cd89982a94345c2bae7cdf51c150e7f793e2f24
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-11-17 05:03:12 +0100

    settings dropdown, proper postcss config?

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="flex flex-col h-screen overflow-hidden font-sans antialiased text-gray-900 bg-gray-700">
-    <header class="relative z-10 flex items-center justify-between flex-shrink-0 px-4 py-4 bg-gray-700 border-b border-gray-200 sm:px-6 lg:px-8">
-    </header>
-    <main class="flex-1 overflow-auto bg-gray-50">
-    </main>
+  <div class="flex flex-col h-screen overflow-hidden font-sans antialiased text-gray-900 bg-gray-900">
+    <div class="flex bg-gray-900 items-center h-16 px-3 sm:px-5 flex-shrink-0 space-x-2">
+      <div class="flex-grow">
+      </div>
+
+      <div class="relative z-20">
+	<Menu>
+	  <span class="rounded-md shadow-sm">
+	    <MenuButton
+		ref="trigger"
+		class="flex items-center text-xs space-x-2 focus:outline-none border-border-transparent focus:border-gray-600 rounded p-1 relative">
+	    <span class="hidden sm:block text-white">
+	      Preferences
+	    </span>
+	      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white fill-current stroke-1" viewBox="0 0 24 24">
+		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="0" d="M19 9l-7 7-7-7" />
+	      </svg>
+	    </MenuButton>
+	  </span>
+
+	  <MenuItems
+	      ref="container"
+	      class="absolute right-0 w-56 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none">
+	  <div class="px-4 py-3">
+	    <p class="text-sm leading-5">Signed in as</p>
+	    <p class="text-sm font-medium leading-5 text-gray-900 truncate">hamros@tuta.io</p>
+	  </div>
+	  </MenuItems>
+	</Menu>
+      </div>
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+	transparent: 'transparent',
+	current: 'currentColor',
+	bluegray: colors.blueGray,
+	coolgray: colors.coolGray,
+	gray: colors.gray,
+	truegray: colors.trueGray,
+	warmgray: colors.warmGray,
+	red: colors.red,
+	orange: colors.orange,
+	amber: colors.amber,
+	yellow: colors.yellow,
+	lime: colors.lime,
+	green: colors.green,
+	emerald: colors.emerald,
+	teal: colors.teal,
+	cyan: colors.cyan,
+	sky: colors.sky,
+	blue: colors.blue,
+	indigo: colors.indigo,
+	violet: colors.violet,
+	purple: colors.purple,
+	fuchsia: colors.fuchsia,
+	pink: colors.pink,
+	rose: colors.rose,
+      }
+    },
   },
   variants: {
     extend: {},
