02de3140217548134747cdd9963f0065d43713d9
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-11-21 05:28:04 +0100

    complex:ish css layout to try to cure my pathological incompetence

--- src/HomeComponent.vue
+++ src/HomeComponent.vue
@@ -1,64 +1,236 @@
 <template>
-  <div class="min-h-[100vh]">
-    <div class="relative overflow-hidden">
-      <div class="absolute wh-full z-[-10]">
-	<div class="pt-9/16">
-	</div>
+  <nav class="absolute inset-x-0 top-[0px]"></nav>
+  <div class="min-h-screen">
+    <!-- -->
+    <div class="relative">
+      <!-- -->
+      <div class="absolute inset-x-0 top-[0px]"></div>
+      <!-- -->
+      <div class="relative flex flex-col justify-end">
+        <div class="pt-[47px]">
+          <div class="relative z-1">
+            <div
+              class="
+                min-h-full
+                wh-full
+                block
+                bg-gradient-to-br
+                from-rose-500
+                to-pink-700
+              "
+            >
+              <div class="py-[300px]"></div>
+            </div>
+            <div class="absolute inset-x-0 bottom-[0px]">
+              <!-- -->
+              <div class="relative text-left p-[35px]"></div>
+              <!-- -->
+              <div
+                class="
+                  d-flex
+                  flex-wrap
+                  justify-start
+                  items-center
+                  flex-wrap
+                  p-[35px]
+                "
+              ></div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="absolute inset-0 bg-cover bg-top">
+    </div>
+    <!-- -->
+    <div class="relative z-1">
+      <div class="relative">
+        <!-- -->
+        <div class="absolute inset-0"></div>
+        <!-- -->
+        <div class="relative border border-transparent content-box">
+          <div class="relative py-[60px]">
+            <div class="relative px-[15px] mx-auto max-w-[875px]">
+              <div
+                class="
+                  w-full
+                  inline-block
+                  p-[15px]
+                  bg-red-500
+                  border border-transparent
+                  h-[100px]
+                "
+              >
+                <div class="relative group overflow-hidden shadow-2xl">
+                  <div class="relative min-h-full min-w-full">
+                    <div class="absolute inset-0 bg-cover bg-center"></div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="
+                  w-full
+                  inline-block
+                  p-[15px]
+                  w-1/2
+                  bg-green-500
+                  border border-transparent
+                  h-[100px]
+                "
+              >
+                <div class="relative group overflow-hidden shadow-2xl">
+                  <div class="relative min-h-full min-w-full">
+                    <div class="absolute inset-0 bg-cover bg-center"></div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="
+                  w-full
+                  inline-block
+                  p-[15px]
+                  w-1/2
+                  bg-blue-500
+                  border border-transparent
+                  h-[100px]
+                "
+              >
+                <div class="relative group overflow-hidden shadow-2xl">
+                  <div class="relative min-h-full min-w-full">
+                    <div class="absolute inset-0 bg-cover bg-center"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
 
-      <div class="relative d-flex flex-col justify-between min-h-[800px]">
-	<div class="container mx-auto">
-	</div>
+    <div class="relative">
+      <div class="z-[-10] absolute inset-0" style="clip: rect(auto, auto, auto, auto); min-height: 200vh; top: -100vh;">
       </div>
 
       <div class="relative">
-	<div class="absolute inset-0 wh-full">
-	</div>
-	<div class="relative border border-transparent content-box">
-	  <div class="container mx-auto">
-	    <div class="relative">
-	      <div class="d-flex flex-wrap">
-		<div class="w-1/3">
-		  <div class="relative overflow-hidden">
-		    <div class="relative min-h-full min-w-full">
-		    </div>
+        <div class="py-[60px]">
+          <div class="container mx-auto px-[10px]">
+            <div class="inline-block w-1/3 p-[10px]">
+              <div class="relative group overflow-hidden shadow-2xl">
+                <div class="relative min-h-full min-w-full">
+                  <div class="absolute inset-0 bg-cover bg-center"></div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-block w-1/3 p-[10px]">
+              <div class="relative group overflow-hidden shadow-2xl">
+                <div class="relative min-h-full min-w-full">
+                  <div class="absolute inset-0 bg-cover bg-center"></div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-block w-1/3 p-[10px]">
+              <div class="relative group overflow-hidden shadow-2xl">
+                <div class="relative min-h-full min-w-full">
+                  <div class="absolute inset-0 bg-cover bg-center"></div>
+                </div>
 
-		    <div class="absolute inset-0">
-		    </div>
-		  </div>
-		</div>
+                <div class="absolute inset-0 bg-black opacity-10"></div>
 
-		<div class="w-1/3">
-		  <div class="relative overflow-hidden">
-		  </div>
+                <div class="absolute inset-0 d-flex items-end">
+                  <div class="relative flex-1">
+                    <div
+                      class="
+                        absolute
+                        bottom-0
+                        inset-x-0
+                        bg-gradient-to-t
+                        from-black
+                        to-transparent
+                        mx-[-1px]
+                        mb-[-1px]
+                        h-3/2
+                      "
+                    >
+                      <div class="relative">
+                        <div class="px-[20px] py-[10px]"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-block w-1/3 p-[10px]">
+              <div class="relative group overflow-hidden shadow-2xl">
+                <div class="relative min-h-full min-w-full">
+                  <div class="absolute inset-0 bg-cover bg-center"></div>
+                </div>
 
-		  <div class="absolute inset-0">
-		  </div>
-		</div>
+                <div class="absolute inset-0 bg-black opacity-10"></div>
 
-		<div class="w-1/3">
-		  <div class="relative overflow-hidden">
-		  </div>
-		  <div class="absolute inset-0">
-		  </div>
-		</div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
+                <div class="absolute inset-0 d-flex items-end">
+                  <div class="relative flex-1">
+                    <div
+                      class="
+                        absolute
+                        bottom-0
+                        inset-x-0
+                        bg-gradient-to-t
+                        from-black
+                        to-transparent
+                        mx-[-1px]
+                        mb-[-1px]
+                        h-3/2
+                      "
+                    >
+                      <div class="relative">
+                        <div class="px-[20px] py-[10px]"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="inline-block w-1/3 p-[10px]">
+              <div class="relative group overflow-hidden shadow-2xl">
+                <div class="relative min-h-full min-w-full">
+                  <div class="absolute inset-0 bg-cover bg-center"></div>
+                </div>
+
+                <div class="absolute inset-0 bg-black opacity-10"></div>
+
+                <div class="absolute inset-0 d-flex items-end">
+                  <div class="relative flex-1">
+                    <div
+                      class="
+                        absolute
+                        bottom-0
+                        inset-x-0
+                        bg-gradient-to-t
+                        from-black
+                        to-transparent
+                        mx-[-1px]
+                        mb-[-1px]
+                        h-3/2
+                      "
+                    >
+                      <div class="relative">
+                        <div class="px-[20px] py-[10px]"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</template> 
+</template>
 
 <script>
-import { defineComponent, h, ref, onMounted, watchEffect, watch } from 'vue'
-import { RouterLink } from 'vue-router'
+import { defineComponent, h, ref, onMounted, watchEffect, watch } from "vue";
+import { RouterLink } from "vue-router";
 
 export default {
-  name: 'HomeComponent',
-}
+  name: "HomeComponent",
+};
 </script>
--- src/RegisterComponent.vue
+++ src/RegisterComponent.vue
@@ -1,57 +1,225 @@
 <template>
-  <div class="absolute z-0 inset-x-0 h-[calc((var(--vh,1vh)*100)-40px)] overflow-hidden">
-    <div class="absolute mx-auto flex flex-col items-center top-[30vh] left-1/2 -translate-x-1/2 max-w-[80vw]">
+  <div
+    class="
+      absolute
+      z-0
+      inset-x-0
+      h-[calc((var(--vh,1vh)*100)-40px)]
+      overflow-hidden
+    "
+  >
+    <div
+      class="
+        absolute
+        mx-auto
+        flex flex-col
+        items-center
+        top-[30vh]
+        left-1/2
+        -translate-x-1/2
+        max-w-[80vw]
+      "
+    >
       <div class="relative">
-	<div class="relative px-[32px] py-[84px]">
-	</div>
+        <div class="relative px-[32px] py-[84px]"></div>
       </div>
     </div>
-    <div class="absolute inset-x-0 top-[11vh] left-[50%] translate-x-[-50%] w-[810px]">
-      <div class="relative overflow-x-scroll lg:overflow-x-visible hide-scrollbar">
-	<div class="inline-flex lg:flex lg:flex-wrap lg:p-0 md:p-[80px] sm:p-[28px]">
-	  <div v-for="index in 6" :key="index" class="relative flex flex-shrink-0 translate-z-[0px] max-w-[270px] p-[5px] w-[270px]">
-	    <div class="relative flex flex-1 flex-col shadow-2xl">
-	      <div class="relative pb-[50%] overflow-hidden">
-		<div class="absolute min-w-full min-h-full object-cover -z-10"></div>
-	      </div>
-	      <div class="relative flex flex-1 flex-col justify-between p-2 min-h-[100px]">
-	      </div>
-	    </div>
-	  </div>
-	</div>
+    <div
+      class="
+        absolute
+        inset-x-0
+        top-[11vh]
+        left-[50%]
+        translate-x-[-50%]
+        w-[810px]
+      "
+    >
+      <div
+        class="relative overflow-x-scroll lg:overflow-x-visible hide-scrollbar"
+      >
+        <div
+          class="
+            inline-flex
+            lg:flex lg:flex-wrap lg:p-0
+            md:p-[80px]
+            sm:p-[28px]
+          "
+        >
+          <div
+            v-for="index in 6"
+            :key="index"
+            class="
+              relative
+              flex flex-shrink-0
+              translate-z-[0px]
+              max-w-[270px]
+              p-[5px]
+              w-[270px]
+            "
+          >
+            <div class="relative flex flex-1 flex-col shadow-2xl">
+              <div class="relative pb-[50%] overflow-hidden">
+                <div
+                  class="absolute min-w-full min-h-full object-cover -z-10"
+                ></div>
+              </div>
+              <div
+                class="
+                  relative
+                  flex flex-1 flex-col
+                  justify-between
+                  p-2
+                  min-h-[100px]
+                "
+              ></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="relative z-10 flex-auto flex items-center justify-center text-sm text-center text-gray-600 py-16 px-4 sm:px-6 lg:px-8">
+    <div
+      class="
+        relative
+        z-10
+        flex-auto flex
+        items-center
+        justify-center
+        text-sm text-center text-gray-600
+        py-16
+        px-4
+        sm:px-6
+        lg:px-8
+      "
+    >
       <div class="w-full max-w-sm">
-	<form action="/login" method="POST">
-	  <div class="relative">
-	    <label class="sr-only">Email address</label>
-	    <input id="email-address" class="text-gray-900 ring-gray-900 ring-opacity-5 placeholder-gray-400 appearance-none bg-white rounded-md block w-full px-3 py-2 border border-transparent shadow ring-1 sm:text-sm mb-4 focus:border-[#22b088] focus:border-opacity-60 focus:ring-[#208f71] focus:outline-none" type="password" placeholder="Email address">
-	  </div>
-	  <div class="relative">
-	    <label class="sr-only">Password</label>
-	    <input id="password" class="text-gray-900 ring-gray-900 ring-opacity-5 placeholder-gray-400 appearance-none bg-white rounded-md block w-full px-3 py-2 border border-transparent shadow ring-1 sm:text-sm mb-4 focus:border-[#22b088] focus:border-opacity-60 focus:ring-[#208f71] focus:outline-none" type="password" placeholder="Password">
-	  </div>
-	  <div class="relative">
-	    <label class="sr-only">Confirm password</label>
-	    <input id="password_confirmation" class="text-gray-900 ring-gray-900 ring-opacity-5 placeholder-gray-400 appearance-none bg-white rounded-md block w-full px-3 py-2 border border-transparent shadow ring-1 sm:text-sm mb-4 focus:border-[#22b088] focus:border-opacity-60 focus:ring-[#208f71] focus:outline-none" type="password" placeholder="Confirm password">
-	  </div>
+        <form action="/login" method="POST">
+          <div class="relative">
+            <label class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              class="
+                text-gray-900
+                ring-gray-900 ring-opacity-5
+                placeholder-gray-400
+                appearance-none
+                bg-white
+                rounded-md
+                block
+                w-full
+                px-3
+                py-2
+                border border-transparent
+                shadow
+                ring-1
+                sm:text-sm
+                mb-4
+                focus:border-[#22b088]
+                focus:border-opacity-60
+                focus:ring-[#208f71]
+                focus:outline-none
+              "
+              type="password"
+              placeholder="Email address"
+            />
+          </div>
+          <div class="relative">
+            <label class="sr-only">Password</label>
+            <input
+              id="password"
+              class="
+                text-gray-900
+                ring-gray-900 ring-opacity-5
+                placeholder-gray-400
+                appearance-none
+                bg-white
+                rounded-md
+                block
+                w-full
+                px-3
+                py-2
+                border border-transparent
+                shadow
+                ring-1
+                sm:text-sm
+                mb-4
+                focus:border-[#22b088]
+                focus:border-opacity-60
+                focus:ring-[#208f71]
+                focus:outline-none
+              "
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="relative">
+            <label class="sr-only">Confirm password</label>
+            <input
+              id="password_confirmation"
+              class="
+                text-gray-900
+                ring-gray-900 ring-opacity-5
+                placeholder-gray-400
+                appearance-none
+                bg-white
+                rounded-md
+                block
+                w-full
+                px-3
+                py-2
+                border border-transparent
+                shadow
+                ring-1
+                sm:text-sm
+                mb-4
+                focus:border-[#22b088]
+                focus:border-opacity-60
+                focus:ring-[#208f71]
+                focus:outline-none
+              "
+              type="password"
+              placeholder="Confirm password"
+            />
+          </div>
 
-	  <button class="block w-full py-2 px-3 border border-[#5cceb5] border-opacity-60 rounded-md text-[#ecf4f2] font-medium tracking-wide bg-gradient-to-t bg-gray-700 to bg-gray-800 shadow-sm sm:text-sm mb-10 hover:bg-gray-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-[#2daa97] focus-visible:ring-offset-2 focus-visible:ring-offset-[#caf0ef]">
-	    Create your account
-	  </button>
-	</form>
+          <button
+            class="
+              block
+              w-full
+              py-2
+              px-3
+              border border-[#5cceb5] border-opacity-60
+              rounded-md
+              text-[#ecf4f2]
+              font-medium
+              tracking-wide
+              bg-gradient-to-t bg-gray-700
+              to
+              bg-gray-800
+              shadow-sm
+              sm:text-sm
+              mb-10
+              hover:bg-gray-900
+              focus:outline-none
+              focus-visible:ring-2
+              focus-visible:ring-[#2daa97]
+              focus-visible:ring-offset-2
+              focus-visible:ring-offset-[#caf0ef]
+            "
+          >
+            Create your account
+          </button>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, h, ref, onMounted, watchEffect, watch } from 'vue'
-import { RouterLink } from 'vue-router'
+import { defineComponent, h, ref, onMounted, watchEffect, watch } from "vue";
+import { RouterLink } from "vue-router";
 
 export default {
-  name: 'RegisterComponent',
-}
+  name: "RegisterComponent",
+};
 </script>
